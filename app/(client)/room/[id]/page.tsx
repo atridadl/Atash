e8de8605cf8b8d3776a19822da8f5b133ca76a21
Atridad Lahiji <me@atri.dad>
2023-09-18 00:57:19 -0600

    Re-organized project directories

---
+++ app/(client)/room/[id]/page.tsx
@@ -0,0 +1,9 @@
+import VoteUI from "@/(client)/room/[id]/VoteUI";
+
+export default function Room() {
+  return (
+    <div className="flex flex-col items-center justify-center text-center gap-2">
+      <VoteUI />
+    </div>
+  );
+}
