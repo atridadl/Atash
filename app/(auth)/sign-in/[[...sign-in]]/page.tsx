419ece9185b3eb968123f7acafba2aea4295bee5
Atridad Lahiji <me@atri.dad>
2023-09-19 23:00:00 -0600

    sign-in change

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -3,17 +3,7 @@
 import { SignIn } from "@clerk/nextjs";
 
 const SignInPage = () => (
-  <div style={styles}>
-    <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
-  </div>
+  <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
 );
 
 export default SignInPage;
-
-const styles = {
-  width: "100vw",
-  height: "100vh",
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-};
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -3,17 +3,7 @@
 import { SignUp } from "@clerk/nextjs";
 
 const SignUpPage = () => (
-  <div style={styles}>
-    <SignUp path="/sign-up" routing="path" signInUrl="/sign-in" />
-  </div>
+  <SignUp path="/sign-up" routing="path" signInUrl="/sign-in" />
 );
 
 export default SignUpPage;
-
-const styles = {
-  width: "100vw",
-  height: "100vh",
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-};
