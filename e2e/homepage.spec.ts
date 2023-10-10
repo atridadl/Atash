ad35816eef52cd4ea4f59a6e25a1575d09e0414f
Atridad Lahiji <me@atri.dad>
2023-10-09 22:29:11 -0600

    Modified BASE URL logic

--- e2e/homepage.spec.ts
+++ e2e/homepage.spec.ts
@@ -1,12 +1,8 @@
 import { test, expect } from "@playwright/test";
-import "dotenv/config";
-
-const BASE_URL = process.env.E2E_TEST_BASE_URL || "http://localhost:3000";
 
 test.describe("Happy Path", () => {
   test.beforeEach(async ({ page }) => {
-    console.log("✨ BASE_URL", BASE_URL);
-    await page.goto(BASE_URL);
+    await page.goto("/");
   });
 
   test.afterEach(async ({ page }) => {
@@ -31,7 +27,7 @@ test.describe("Happy Path", () => {
       .locator("#password-field")
       .fill(process.env.E2E_TEST_USER_PASSWORD!);
     await page.getByRole("button", { name: "Continue", exact: true }).click();
-    await page.waitForURL(BASE_URL);
+    await page.waitForURL("/");
     await page.locator("#dashboard-button").isVisible();
   });
 });
--- playwright.config.ts
+++ playwright.config.ts
@@ -1,13 +1,18 @@
 import { defineConfig } from "@playwright/test";
+import "dotenv/config";
+
+const BASE_URL = process.env.E2E_TEST_BASE_URL || "http://localhost:3000";
 
 export default defineConfig({
   testDir: "e2e",
-  webServer: {
-    command: "pnpm dev",
-    url: "http://127.0.0.1:3000",
-  },
+  webServer: process.env.E2E_TEST_BASE_URL
+    ? {
+        command: "pnpm dev",
+        url: "http://127.0.0.1:3000",
+      }
+    : undefined,
   use: {
-    baseURL: "http://127.0.0.1:3000",
+    baseURL: BASE_URL,
     trace: "on-first-retry",
   },
 });
