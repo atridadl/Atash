556333b240ed6523f480ab786888c2bea1bae8c5
Atridad Lahiji <me@atri.dad>
2023-10-09 22:32:18 -0600

    other way around

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,11 +6,11 @@ const BASE_URL = process.env.E2E_TEST_BASE_URL || "http://localhost:3000";
 export default defineConfig({
   testDir: "e2e",
   webServer: process.env.E2E_TEST_BASE_URL
-    ? {
+    ? undefined
+    : {
         command: "pnpm dev",
         url: "http://127.0.0.1:3000",
-      }
-    : undefined,
+      },
   use: {
     baseURL: BASE_URL,
     trace: "on-first-retry",
