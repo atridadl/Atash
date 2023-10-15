6b9944b017b7cf3acd6a8e18d328237163a300ff
Atridad Lahiji <me@atri.dad>
2023-10-15 19:48:02 -0300

    Update VoteUI.tsx

--- app/(client)/room/[id]/VoteUI.tsx
+++ app/(client)/room/[id]/VoteUI.tsx
@@ -42,7 +42,6 @@ const VoteUI = () => {
   const {
     data: roomFromDb,
     isLoading: roomFromDbLoading,
-    isFetching: roomFromDbFetching,
   } = useQuery({
     queryKey: ["room"],
     queryFn: getRoomHandler,
@@ -318,7 +317,7 @@ const VoteUI = () => {
   // UI
   // =================================
   // Room is loading
-  if (roomFromDbLoading || roomFromDbFetching) {
+  if (roomFromDbLoading) {
     return <LoadingIndicator />;
     // Room has been loaded
   } else {
