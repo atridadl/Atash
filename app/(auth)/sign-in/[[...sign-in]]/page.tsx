f54c687797105e5d2733343a321db74d3cfcb35a
atridadl <me@atri.dad>
2023-11-13 18:12:52 -0700

    Central Auth

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,15 @@
 import { SignIn } from "@clerk/nextjs";
+import { env } from "env.mjs";
 
 export const runtime = "edge";
 
 const SignInPage = () => (
-  <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+  <SignIn
+    path="/sign-in"
+    routing="path"
+    signUpUrl="/sign-up"
+    redirectUrl={env.ROOT_URL ? env.ROOT_URL : "/"}
+  />
 );
 
 export default SignInPage;
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,9 +1,15 @@
 import { SignUp } from "@clerk/nextjs";
+import { env } from "env.mjs";
 
 export const runtime = "edge";
 
 const SignUpPage = () => (
-  <SignUp path="/sign-up" routing="path" signInUrl="/sign-in" />
+  <SignUp
+    path="/sign-up"
+    routing="path"
+    signInUrl="/sign-in"
+    redirectUrl={env.ROOT_URL ? env.ROOT_URL : "/"}
+  />
 );
 
 export default SignUpPage;
