d3ff4d7ebd558ad581c720b1a16ab65f340c5179
atridadl <me@atri.dad>
2023-11-30 23:23:15 -0700

    Sign in fixes

--- app/routes/sign-in.$.tsx
+++ app/routes/sign-in.$.tsx
@@ -1,7 +1,5 @@
 import { SignIn } from "@clerk/remix";
 
 export default function SignInPage() {
-  return (
-    <SignIn redirectUrl={process.env.ROOT_URL ? process.env.ROOT_URL : "/"} />
-  );
+  return <SignIn />;
 }
--- app/routes/sign-up.$.tsx
+++ app/routes/sign-up.$.tsx
@@ -1,7 +1,5 @@
 import { SignUp } from "@clerk/remix";
 
 export default function SignUpPage() {
-  return (
-    <SignUp redirectUrl={process.env.ROOT_URL ? process.env.ROOT_URL : "/"} />
-  );
+  return <SignUp />;
 }
